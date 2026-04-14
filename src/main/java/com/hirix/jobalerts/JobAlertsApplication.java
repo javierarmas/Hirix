337a2bc2c1840dbfb0c26a28e34816b4e4af566d
Javier <javierarmas499@gmail.com>
2026-04-14 03:13:36 -0600

    refactor: externalize sensitive configuration to environment variables

--- src/main/java/com/hirix/jobalerts/JobAlertsApplication.java
+++ src/main/java/com/hirix/jobalerts/JobAlertsApplication.java
@@ -5,12 +5,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableScheduling
 public class JobAlertsApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+
+        // SENDGRID
+        System.setProperty("SENDGRID_API_KEY", dotenv.get("SENDGRID_API_KEY"));
+        System.setProperty("SENDGRID_FROM_EMAIL", dotenv.get("SENDGRID_FROM_EMAIL"));
+        System.setProperty("ALERT_TO_EMAIL", dotenv.get("ALERT_TO_EMAIL"));
+
+        // DB
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        // ADZUNA
+        System.setProperty("ADZUNA_APP_ID", dotenv.get("ADZUNA_APP_ID"));
+        System.setProperty("ADZUNA_APP_KEY", dotenv.get("ADZUNA_APP_KEY"));
+        System.setProperty("ADZUNA_COUNTRY", dotenv.get("ADZUNA_COUNTRY"));
+
         SpringApplication.run(JobAlertsApplication.class, args);
+
     }
 }
